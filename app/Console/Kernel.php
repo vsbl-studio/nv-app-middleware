fef464ff1fa5766bf1a7d93725e7d8fd4eb63999
liudas-neat <liudas@itsneat.digital>
2026-08-26 09:59:04 +0300

    Add overlapping mutex to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update-data')->hourly();
+        $schedule->command('update-data')->hourly()->withoutOverlapping();
     }
 
     /**
